d83562be168bb75bc707ead19c3a7d8865e6e77e
ajugeorge98 <54764698+ajugeorge98@users.noreply.github.com>
2023-02-03 17:52:14 +0530

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
 # Some general variables
+PHONE="x3pro"
 ARCH="arm64"
 SUBARCH="arm64"
-DEFCONFIG=nogravity_defconfig
+DEFCONFIG=vayu_user_defconfig
 COMPILER=clang
 LINKER="lld"
-COMPILERDIR="/media/pierre/Expension/Android/PocoX3Pro/Kernels/Proton-Clang"
+COMPILERDIR="$GITHUB_WORKSPACE/kernel_workspace/tools/clang/host/linux-x86/clang-r428724" 
+export PATH="$GITHUB_WORKSPACE/kernel_workspace/tools/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH" 
+export PATH="$GITHUB_WORKSPACE/kernel_workspace/tools/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH" 
+export LD_LIBRARY_PATH="$GITHUB_WORKSPACE/kernel_workspace/tools/clang/host/linux-x86/clang-r428724/lib64:$LD_LIBRARY_PATH"
+# Cleanup output
+rm -rf out/outputs/${PHONE}/*
 
 # Export shits
-export KBUILD_BUILD_USER=Pierre2324
-export KBUILD_BUILD_HOST=G7-7588
+export KBUILD_BUILD_USER=
+export KBUILD_BUILD_HOST=
 
 # Speed up build process
 MAKE="./makeparallel"
@@ -28,9 +34,10 @@ PATH="${COMPILERDIR}/bin:${PATH}" \
 make -j$(nproc --all) O=out \
 ARCH=${ARCH} \
 CC=${COMPILER} \
-CROSS_COMPILE=${COMPILERDIR}/bin/aarch64-linux-gnu- \
-CROSS_COMPILE_ARM32=${COMPILERDIR}/bin/arm-linux-gnueabi- \
-LD_LIBRARY_PATH=${COMPILERDIR}/lib \
+CLANG_TRIPLE=aarch64-linux-gnu- \
+CROSS_COMPILE=$GITHUB_WORKSPACE/kernel_workspace/tools/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel_workspace/tools/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-gnueabi- \
+LD_LIBRARY_PATH=$GITHUB_WORKSPACE/kernel_workspace/tools/clang/host/linux-x86/clang-r428724/lib64 \
 Image.gz-dtb dtbo.img
 }
 
@@ -39,8 +46,9 @@ PATH="${COMPILERDIR}/bin:${PATH}" \
 make -j$(nproc --all) O=out \
 ARCH=${ARCH} \
 CC=${COMPILER} \
-CROSS_COMPILE=${COMPILERDIR}/bin/aarch64-linux-gnu- \
-CROSS_COMPILE_ARM32=${COMPILERDIR}/bin/arm-linux-gnueabi- \
+CLANG_TRIPLE=aarch64-linux-gnu- \
+CROSS_COMPILE=$GITHUB_WORKSPACE/kernel_workspace/tools/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel_workspace/tools/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-gnueabi- \
 LD=ld.${LINKER} \
 AR=llvm-ar \
 NM=llvm-nm \
@@ -48,8 +56,21 @@ OBJCOPY=llvm-objcopy \
 OBJDUMP=llvm-objdump \
 STRIP=llvm-strip \
 ld-name=${LINKER} \
-KBUILD_COMPILER_STRING="Proton Clang" \
+KBUILD_COMPILER_STRING="Proton-Clang" \
 Image.gz-dtb dtbo.img
+}
+
+miui_fix_dimens() {
+    sed -i 's/<70>/<695>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-36-02-0a-lcd-dsc-vid.dtsi
+    sed -i 's/<154>/<1546>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-36-02-0a-lcd-dsc-vid.dtsi
+    sed -i 's/<70>/<695>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-42-02-0b-lcd-dsc-vid.dtsi
+    sed -i 's/<154>/<1546>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-42-02-0b-lcd-dsc-vid.dtsi
+}
+restore_dimens() {
+    sed -i 's/<695>/<70>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-36-02-0a-lcd-dsc-vid.dtsi
+    sed -i 's/<1546>/<154>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-36-02-0a-lcd-dsc-vid.dtsi
+    sed -i 's/<695>/<70>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-42-02-0b-lcd-dsc-vid.dtsi
+    sed -i 's/<1546>/<154>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-42-02-0b-lcd-dsc-vid.dtsi
 }
 
 # Make defconfig
@@ -75,6 +96,22 @@ then
     echo "Build failed"
 else
     echo "Build succesful"
+    mkdir out/outputs
+    mkdir out/outputs/${PHONE}
+    find out/arch/arm64/boot/dts/qcom/ -name '*.dtb' -exec cat {} + >out/outputs/${PHONE}/dtb
+    cp out/arch/arm64/boot/dtbo.img out/outputs/${PHONE}/dtbo.img
+    cp out/arch/arm64/boot/Image.gz out/outputs/${PHONE}/Image.gz
+    #MIUI dtbo
+    rm out/outputs/dtbo-miui.img
+    miui_fix_dimens
+    echo | Build_lld
+    if [ $? -ne 0 ]
+    then
+        rm out/outputs/dtbo-miui.img
+    else
+        cp out/arch/arm64/boot/dtbo.img out/outputs/dtbo-miui.img
+    fi
+    restore_dimens
 fi
 
 BUILD_END=$(date +"%s")
